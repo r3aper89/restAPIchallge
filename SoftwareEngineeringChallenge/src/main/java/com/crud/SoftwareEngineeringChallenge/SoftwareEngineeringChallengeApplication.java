dd6b1777d2cd7a5aefe66846146b5366b190e51b
r3aper89 <56981798+r3aper89@users.noreply.github.com>
2023-08-04 08:45:46 +0930

    feat: setup project

---
+++ SoftwareEngineeringChallenge/src/main/java/com/crud/SoftwareEngineeringChallenge/SoftwareEngineeringChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.crud.SoftwareEngineeringChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineeringChallengeApplication.class, args);
+	}
+
+}
